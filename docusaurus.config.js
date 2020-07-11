fa4600be974b38a2eeae5b0e271cb5e17c925773
Mostafa Farghaly <keepondev@gmail.com>
2020-07-11 17:19:23 +0400

    updated footer with copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ module.exports = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Kotal.`,
     },
   },
   presets: [
