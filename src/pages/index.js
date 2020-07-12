c99aa4ec856d34188a69d00d059b8b6db424001b
Mostafa Farghaly <keepondev@gmail.com>
2020-07-12 16:16:02 +0400

    updated getting started button style

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ function Home() {
           <div className={styles.buttons}>
             <Link
               className={clsx(
-                'button button--secondary button--lg',
+                'button button--success button--lg',
                 styles.getStarted,
               )}
               to={useBaseUrl('docs/')}>
