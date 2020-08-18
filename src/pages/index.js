636d02816046194a7a60583b340ef63b4736f2d9
Mostafa Farghaly <keepondev@gmail.com>
2020-08-18 16:06:03 +0400

    changed title and description

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,8 +59,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.tagline}
+      description={siteConfig.title}>
       <header className={clsx('hero', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
